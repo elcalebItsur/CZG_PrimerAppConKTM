9e15d687132a5276113aab9b4360c3f18ea0fa77
elcalebitsur <s22120212@alumnos.itsur.edu.mx>
2026-05-07 00:57:22 -0600

    main.kt, actualización

--- composeApp/src/jvmMain/kotlin/com/example/czg_primerappconktm/main.kt
+++ composeApp/src/jvmMain/kotlin/com/example/czg_primerappconktm/main.kt
@@ -1,12 +1,22 @@
 package com.example.czg_primerappconktm
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
+    val state = rememberWindowState(
+        size = DpSize(400.dp, 350.dp),
+        position = WindowPosition(300.dp, 300.dp)
+    )
     Window(
+        title = "Local Time App",
         onCloseRequest = ::exitApplication,
-        title = "CZG_PrimerAppConKTM",
+        state = state,
+        alwaysOnTop = true
     ) {
         App()
     }
